821bc5a27e9da0424ca2c7e1fb5b4cbe624c4315
Rondinelli França <rondinelli.franca@gmail.com>
2022-04-06 21:39:36 -0300

    Criação dos controles sem função

---
+++ tcc.pos.puc.boasaude/src/4 -Apresentacao/tcc.pos.puc.boasaude.api/Controllers/ConveniadosController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tcc.pos.puc.boasaude.api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConveniadosController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+         {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+        [HttpGet(Name = "ConveniadosController")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
